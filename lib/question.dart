8b01cb7dd94ccbcb1878d626e46246ff205920b7
Harshit Tathagat <harshittathagat@gmail.com>
2022-04-28 12:52:05 +0530

    Quiz App

---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+  // Dart modified constructor
+  Question(this.questionText, this.questionAnswer);
+  /*
+  * We can also intialize it in the traditional format
+  * Question(String questionText,bool questionAnswer){
+  * this.questionText=questionText;
+  * this.questionAnswer=questionAnswer;
+  * }
+  * */
+}
