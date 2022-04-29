75da3d47b9b0c589509de689aee24a20a6283276
Harshit Tathagat <harshittathagat@gmail.com>
2022-04-29 19:13:21 +0530

    Removed Repeated Questions

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -3,9 +3,6 @@ import 'question.dart';
 class QuizBrain {
   int _questionNumber = 0;
   List<Question> _questionBank = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
